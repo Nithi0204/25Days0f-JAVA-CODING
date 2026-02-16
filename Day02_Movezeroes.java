a3fd2174584c6e92c560ed284aa0b20f0425ceb6
Nithi0204 <147030747+Nithi0204@users.noreply.github.com>
2026-02-16 12:47:40 +0530

    Day02_Movezeroes.java

--- Day02_Movezeroes.java
+++ Day02_Movezeroes.java
@@ -12,10 +12,10 @@ public static void moveZeroes(int[] nums) {
         for(i=i;i<nums.length;i++){
             nums[i]=0;
         }
-        
-    }
+}
     public static void main(String[] args){
     int[] nums={0,1,0,3,12};
     moveZeroes(nums);
     System.out.println(Arrays.toString(nums));
     }
+}
